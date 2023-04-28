7fd5edffe24256f640880057ba2d5bd6d17fed32
Fizioh <batrom@laposte.net>
2023-04-28 09:32:41 +0200

    feat: add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Navbar() {
+    return(
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" sx={{backgroundColor: "#1b1a1a"}}>
+            <Toolbar sx={{display: 'flex', justifyContent:"center"}}>
+                <Button color="inherit">
+                    <Typography variant="subtitle2" component="div" sx={{ flexGrow: 1 }}>
+                    Projets
+                    </Typography>
+                </Button>
+                <Button color="inherit">
+                    <Typography variant="subtitle2" component="div" sx={{ flexGrow: 1 }}>
+                    Contact
+                    </Typography>
+                </Button>
+            </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
